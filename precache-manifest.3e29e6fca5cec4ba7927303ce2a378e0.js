6aa42a7a31a4ea1f8cab6015a95f0b07ae2eeaa3
pravinsathishprabu <pravin592002@gmail.com>
2023-12-31 16:29:55 +0530

    Updates

---
+++ precache-manifest.3e29e6fca5cec4ba7927303ce2a378e0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebc9bc5588bf34fe9ec2",
+    "url": "/portfolio/static/css/main.2e04bd28.chunk.css"
+  },
+  {
+    "revision": "ebc9bc5588bf34fe9ec2",
+    "url": "/portfolio/static/js/main.6558ade7.chunk.js"
+  },
+  {
+    "revision": "c9e9714385779cc8dcec",
+    "url": "/portfolio/static/js/runtime~main.a37041f1.js"
+  },
+  {
+    "revision": "63a92f8d7a5639d1bb7c",
+    "url": "/portfolio/static/css/2.2cdc23e5.chunk.css"
+  },
+  {
+    "revision": "63a92f8d7a5639d1bb7c",
+    "url": "/portfolio/static/js/2.8a62bc24.chunk.js"
+  },
+  {
+    "revision": "93b7003b8b3fe8bac328",
+    "url": "/portfolio/static/js/3.c17d3b5c.chunk.js"
+  },
+  {
+    "revision": "a10b72b0966b51744933103239d7d2df",
+    "url": "/portfolio/static/media/pravin.a10b72b0.png"
+  },
+  {
+    "revision": "a5e2b6af9687321c75a5eb08c48970fe",
+    "url": "/portfolio/static/media/vivid.a5e2b6af.jpg"
+  },
+  {
+    "revision": "4090e4ad9c2e50a98ba1b899fb30f0e2",
+    "url": "/portfolio/static/media/uzhavan.4090e4ad.png"
+  },
+  {
+    "revision": "d2b881061ca8fe7a9e3bc8dd45c1d544",
+    "url": "/portfolio/static/media/mamcet.d2b88106.png"
+  },
+  {
+    "revision": "cd3baf3b592fe8645a4f72463c827c8e",
+    "url": "/portfolio/static/media/symposiyum.cd3baf3b.png"
+  },
+  {
+    "revision": "29ea80c366e0577022ef919a26a42cde",
+    "url": "/portfolio/static/media/webbee.29ea80c3.png"
+  },
+  {
+    "revision": "a1188709cde0abd03464ef76b2b680c5",
+    "url": "/portfolio/static/media/snaplogic.a1188709.webp"
+  },
+  {
+    "revision": "a94f40158864101ad153cf087842a9c3",
+    "url": "/portfolio/index.html"
+  }
+];
